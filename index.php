dcdf478e8202f9c75ced40c70c120e6db782d853
Navya2109 <133104473+Navya2109@users.noreply.github.com>
2025-03-06 12:14:12 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include('header.php'); ?>
+
+<div class="main-content">
+    <h1>Welcome to the Fashion Store</h1>
+    <p>Discover the latest trends in fashion. Browse our collections below!</p>
+
+    <a href="products.php" class="view-all">View All Products</a>
+</div>
+
+<?php include('footer.php'); ?>
